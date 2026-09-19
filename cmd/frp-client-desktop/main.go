4a45317dd2745c4ed1e2b4fc35176b908259b6ad
wanstu <wanstu@163.com>
2026-09-19 19:38:34 +0800

    fix: keep Linux window in tray on close

--- cmd/frp-client-desktop/main.go
+++ cmd/frp-client-desktop/main.go
@@ -44,7 +44,7 @@ func runDesktop(app *App, launch desktopkit.LaunchOptions) error {
 	window.Height = 720
 	window.MinWidth = 860
 	window.MinHeight = 600
-	window.HidePolicy = desktopkit.HideSafe
+	window.HidePolicy = desktopkit.HideAlways
 	window.StartHiddenOnAutoStart = true
 	window.Background = desktopkit.Color{R: 245, G: 247, B: 250, A: 1}
 
